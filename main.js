fe1467fda1106e8bc3ba945f18dc26061109aea1
David Xu <dxaviud@gmail.com>
2021-03-22 23:32:38 -0400

    Ball object

--- main.js
+++ main.js
@@ -3,12 +3,49 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
 
 // function to generate random number
 
 function random(min, max) {
     const num = Math.floor(Math.random() * (max - min + 1)) + min;
     return num;
+}
+
+function Ball(x, y, velX, velY, color, radius) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.radius = radius;
+}
+
+Ball.prototype.draw = function() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+Ball.prototype.update = function() {
+    if ((this.x + this.size) >= width) {
+        this.velX *= -1;
+    }
+
+    if ((this.x - this.size) <= 0) {
+        this.velX *= -1;
+    }
+
+    if ((this.y + this.size) >= height) {
+        this.velY *= -1;
+    }
+
+    if ((this.y - this.size) <= 0) {
+        this.velY *= -1;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
 }
